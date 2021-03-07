bfab4096f3605c7eb5c6a6a9e3c4af7cda87c151
caleb park <calebparky@gmail.com>
2021-03-06 23:32:02 -0600

    recording + interactivity with python scripts

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
 // Import modules
 const http = require('http');
 var express = require('express'); // Make sure to run npm install express
+var bodyParser = require('body-parser');
+const path = require('path');
+const spawn = require("child_process").spawn;
 var app = express();
 
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs'); // npm add ejs
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json())
 // Use static resources
 // https://stackoverflow.com/questions/15316908/how-to-render-css-in-nodejs
 app.use('/static', express.static('static'));
+// app.use(express.static('public'));
 
 app.get('/', function (req, res) {
    res.sendFile(`${__dirname}/views/index.html`);
@@ -20,8 +29,20 @@ app.get('/signup', function (req, res) {
 })
 
 app.get('/patient', function (req, res) {
-   res.sendFile(`${__dirname}/views/patient.html`);
+   res.render('patient');
 })
+
+// we shall use this function to make a call to our machine learning model
+app.get('/test', function (req, res) {
+   const pythonProcess = spawn('python',["test.py", 1, 2]); // cffr: https://stackoverflow.com/questions/23450534/how-to-call-a-python-function-from-node-js
+   pythonProcess.stdout.on('data', (data) => {
+      console.log(data.toString());
+      res.write(data);
+      res.end('end');
+   });
+})
+
+
 
 var server = app.listen(8081, function () {
    var host = server.address().address
---
+++ static/dashboard.js
@@ -0,0 +1,6 @@
+var session = {
+    audio: true,
+    video: false
+};
+var recordRTC = null;
+navigator.getUserMedia(session, initializeRecorder, onError);
